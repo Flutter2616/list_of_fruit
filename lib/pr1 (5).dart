ddbcb9c687608d3736e9c0e24404ac3ded7f2ebf
prince <princerawal225@gmail.com>
2023-02-06 15:14:37 +0530

    pr1

--- lib/pr1 (5).dart
+++ lib/pr1 (5).dart
@@ -1,95 +1,95 @@
 import 'dart:io';
 
 void main() {
-  List<int> l1 = [];
-  int i, j, n,ch,sum=1;
-
-  print("Enter size of array :");
-  n = int.parse(stdin.readLineSync()!);
+  List l1 = [];
+  int i, j;
+  int ch;
+  num sum = 0;
 
   print("\nEnter the Element :");
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
-    {
-      l1.add(int.parse(stdin.readLineSync()!));
+  for (i = 0; i < 3; i++) {
+    List ll = [];
+    for (j = 0; j < 3; j++) {
+      ll.add(int.parse(stdin.readLineSync()!));
     }
+    l1.add(ll);
   }
-
-
   print("\npress '0' for exit");
   print("Press '1' for sum all element");
   print("Press '2' for sum of row");
   print("Press '3' for sum of column");
   print("Press '4' for sum diagonal");
-  print("Press '4' for sum antidiagonal");
+  print("Press '5' for sum antidiagonal");
   do {
+
     print("Enter your respons :");
     ch = int.parse(stdin.readLineSync()!);
+
     switch (ch) {
       case 1:
-        for(i=0;i<n;i++)
+      sum=0;
+        for (i = 0; i < 3; i++)
+        {
+
+          for (j = 0; j < 3; j++)
           {
-            for(j=0;j<n;j++)
-              {
-                sum=sum+l1[j];
-              }
+            sum = sum + l1[i][j];
           }
-        print("sum of all element :${sum}");
+        }
+        print("sum of all element :$sum");
         break;
+      
 
       case 2:
-        for(i=0;i<n;i++)
-        {
-          for(j=0;j<n;j++)
-          {
-            sum=sum+l1[j];
+      sum=0;
+        for (i = 0; i < 3; i++) {
+          sum=0;
+          for (j = 0; j < 3; j++) {
+            sum = sum + l1[i][j];
           }
-          print("sum of ${i} row :${sum}");
+          print("sum of $i row :$sum");
         }
         break;
+      
 
-      // case 3:
-      //   for(i=0;i<n;i++)
-      //   {
-      //     for(j=0;j<n;j++)
-      //     {
-      //       if(j==i-1)
-      //       {
-      //         sum = sum + l1[j];
-      //       }
-      //         }
-      //     print("sum of ${i} column :${sum}");
-      //   }
-      //   break;
+      case 3:
+      sum=0;
+        for (i = 0; i < 3; i++) {
+          sum=0;
+          for (j = 0; j < 3; j++) {
+            sum = sum + l1[j][i];
+            // print("hello");
+          }
+          print("sum of $i column :$sum");
+        }
+        break;
+      
 
       case 4:
-        for(i=0;i<n;i++)
-        {
-          for(j=0;j<n;j++)
-          {
-            if(j==i)
-            {
-              sum = sum + l1[j];
+      sum=0;
+        for (i = 0; i < 3; i++) {
+          for (j = 0; j < 3; j++) {
+            if (j == i) {
+              sum = sum + l1[i][j];
             }
           }
-          print("sum of diagonal :${sum}");
         }
+        print("sum of diagonal :$sum");
         break;
+      
 
       case 5:
-        for(i=0;i<n;i++)
-        {
-          for(j=0;j<n;j++)
-          {
-            if(i+j==n)
-            {
-              sum = sum + l1[j];
+      sum=0;
+        for (i = 0; i < 3; i++) {
+          for (j = 0; j < 3; j++) {
+            if (i + j == 2) {
+              sum = sum + l1[i][j];
             }
           }
-          print("sum of ${i} antidiagonal :${sum}");
         }
+      print("sum of antidiagonal :$sum");
         break;
+
 
       case 0:
         print("Thank you for responce");
@@ -98,5 +98,6 @@ void main() {
       default:
         print("invalid number..!!\n\npleace enter valid number..!!");
     }
+
   } while (ch != 0);
 }
